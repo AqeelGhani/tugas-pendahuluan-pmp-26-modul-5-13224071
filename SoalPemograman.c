8814bd2f2893cc8cb0b58f1ad7b0e61118598e8a
Aqeel Ghani <aqeel70106@gmail.com>
2026-05-07 22:21:38 +0700

    feat : Pembuatan fungsi LCA

--- SoalPemograman.c
+++ SoalPemograman.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 // Struktur data untuk Node Tree nya
@@ -11,6 +12,25 @@ typedef struct Node{
     char Nama[100];
     struct Node *anak1, *anak2;
 } Node;
+
+// Pembuatan fungsi LCA (Lowest Common Ancestor)
+Node* LCA (Node *root, char *nama1, char *nama2){
+    // Jika merupakan ujung tree
+    if (root == NULL) return NULL;
+
+    // Jika root merupakan salah satu keturunan yang dicari
+    if ((strcmp(root->Nama, nama1) == 0) || (strcmp(root->Nama, nama1) == 0)) return root;
+
+    Node *anak1LCA = LCA(root->anak1, nama1, nama2);
+    Node *anak2LCA = LCA(root->anak2, nama1, nama2);
+
+    // Jika kedua anak merupakan leluhur dari keturunan yang dicari, maka root ini merupakan LCA
+    // (Berjalan/terpenuhi jika kedua variabel bukan NULL)
+    if (anak1LCA && anak2LCA) return root;
+
+    // Mengembalikan pointer anak yang bukan NULL (Jika ada)
+    return (anak1LCA != NULL) ? anak1LCA : anak2LCA;
+}
 
 int main(){
 
