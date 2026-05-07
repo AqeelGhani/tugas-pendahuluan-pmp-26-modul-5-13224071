c6775dc2cbb8d0da2ae9656fcd78b461b3d3a463
Aqeel Ghani <aqeel70106@gmail.com>
2026-05-07 22:05:40 +0700

    feat : penambahan struktur data Node

--- SoalPemograman.c
+++ SoalPemograman.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// Struktur data untuk Node Tree nya
+typedef struct Node{
+    char Nama[100];
+    struct Node *anak1, *anak2;
+} Node;
+
 int main(){
 
 }
+
+// Referensi
+// https://www.geeksforgeeks.org/dsa/lowest-common-ancestor-binary-tree-set-1/
